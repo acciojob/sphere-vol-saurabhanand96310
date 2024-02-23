3a7fc3760e4cf658d28c846c463f385b23e50d0b
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-23 16:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	let radius =document.querySelector("#radius");
+	let volume=document.querySelector("#volume");
+	let r=radius.value;
+	if(r>0){
+		let pi=3.14;
+		let v=4/3*pi*r*r*r;
+		volume.value=v;
+	}else{
+		volume.value=NaN;
+	}
   
 } 
 
